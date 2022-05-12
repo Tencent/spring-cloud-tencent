481eac54eb3f88c12929f1c28a078153a6bea0ad
lepdou <ledouzhang@tencent.com>
2022-05-12 15:12:23 +0800

    override recover router config

---
+++ spring-cloud-starter-tencent-polaris-discovery/src/main/java/com/tencent/cloud/polaris/DiscoveryConfigModifier.java
@@ -0,0 +1,50 @@
+/*
+ * Tencent is pleased to support the open source community by making Spring Cloud Tencent available.
+ *
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ */
+
+package com.tencent.cloud.polaris;
+
+import com.tencent.cloud.common.constant.ContextConstant;
+import com.tencent.cloud.polaris.context.PolarisConfigModifier;
+import com.tencent.polaris.api.config.consumer.ServiceRouterConfig;
+import com.tencent.polaris.factory.config.ConfigurationImpl;
+import com.tencent.polaris.plugins.router.healthy.RecoverRouterConfig;
+
+/**
+ *  Spring Cloud Tencent config Override polaris config.
+ *
+ *@author lepdou 2022-04-24
+ */
+public class DiscoveryConfigModifier implements PolarisConfigModifier {
+
+	@Override
+	public void modify(ConfigurationImpl configuration) {
+		// Set excludeCircuitBreakInstances to false
+		RecoverRouterConfig recoverRouterConfig = configuration.getConsumer().getServiceRouter()
+				.getPluginConfig(ServiceRouterConfig.DEFAULT_ROUTER_RECOVER, RecoverRouterConfig.class);
+		recoverRouterConfig.setExcludeCircuitBreakInstances(false);
+
+		// Update modified config to source properties
+		configuration.getConsumer().getServiceRouter()
+				.setPluginConfig(ServiceRouterConfig.DEFAULT_ROUTER_RECOVER, recoverRouterConfig);
+	}
+
+	@Override
+	public int getOrder() {
+		return ContextConstant.ModifierOrder.DISCOVERY_ORDER;
+	}
+}
--- spring-cloud-starter-tencent-polaris-discovery/src/main/java/com/tencent/cloud/polaris/DiscoveryPropertiesAutoConfiguration.java
+++ spring-cloud-starter-tencent-polaris-discovery/src/main/java/com/tencent/cloud/polaris/DiscoveryPropertiesAutoConfiguration.java
@@ -72,6 +72,11 @@ public class DiscoveryPropertiesAutoConfiguration {
 		return new PolarisDiscoveryHandler();
 	}
 
+	@Bean
+	public DiscoveryConfigModifier discoveryConfigModifier() {
+		return new DiscoveryConfigModifier();
+	}
+
 	@PostConstruct
 	public void init() {
 		if (null != polarisDiscoveryProperties) {
