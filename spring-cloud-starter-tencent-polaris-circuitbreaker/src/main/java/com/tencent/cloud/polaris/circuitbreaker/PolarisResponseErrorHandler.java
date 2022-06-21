505de5f0ba9c6725d08354595a14a71f7db3c098
weihu <weihubeats@163.com>
2022-06-21 19:18:22 +0800

    add restTemplate Report Polaris

---
+++ spring-cloud-starter-tencent-polaris-circuitbreaker/src/main/java/com/tencent/cloud/polaris/circuitbreaker/PolarisResponseErrorHandler.java
@@ -0,0 +1,11 @@
+package com.tencent.cloud.polaris.circuitbreaker;
+
+import org.springframework.web.client.ResponseErrorHandler;
+
+/**
+ * @author : wh
+ * @date : 2022/6/21 19:12
+ * @description:
+ */
+public interface PolarisResponseErrorHandler extends ResponseErrorHandler {
+}
