3c4bddd6d1b02461531427124748dd88ee1e716e
Haotian Zhang <928016560@qq.com>
2022-07-18 13:40:37 +0800

    feat:enhance Feign and RestTemplate and support Polaris monitor. (#435)
    Co-authored-by: VOPEN.XYZ <x_vivi@yeah.net>

---
+++ spring-cloud-tencent-rpc-enhancement/src/test/java/com/tencent/cloud/rpc/enhancement/stat/config/PolarisStatPropertiesTest.java
@@ -0,0 +1,57 @@
+/*
+ * Tencent is pleased to support the open source community by making Spring Cloud Tencent available.
+ *
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.tencent.cloud.rpc.enhancement.stat.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Test for {@link PolarisStatProperties}.
+ *
+ * @author Haotian Zhang
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = PolarisStatPropertiesTest.TestApplication.class)
+@ActiveProfiles("test")
+public class PolarisStatPropertiesTest {
+
+	@Autowired
+	private PolarisStatProperties polarisStatProperties;
+
+	@Test
+	public void testDefaultInitialization() {
+		assertThat(polarisStatProperties).isNotNull();
+		assertThat(polarisStatProperties.isEnabled()).isTrue();
+		assertThat(polarisStatProperties.getHost()).isNotBlank();
+		assertThat(polarisStatProperties.getPort()).isEqualTo(20000);
+		assertThat(polarisStatProperties.getPath()).isEqualTo("/xxx");
+	}
+
+	@SpringBootApplication
+	protected static class TestApplication {
+
+	}
+}
