9cd0040e8cd856f4bb894f290283a9e99031e6ad
pandaapo <35672972+pandaapo@users.noreply.github.com>
2022-08-04 10:17:44 +0800

    Fix the code analysis error.

--- spring-cloud-starter-tencent-polaris-config/src/main/java/com/tencent/cloud/polaris/config/adapter/PolarisPropertySourceAutoRefresher.java
+++ spring-cloud-starter-tencent-polaris-config/src/main/java/com/tencent/cloud/polaris/config/adapter/PolarisPropertySourceAutoRefresher.java
@@ -84,7 +84,7 @@ public class PolarisPropertySourceAutoRefresher implements ApplicationListener<A
 				public void onChange(ConfigKVFileChangeEvent configKVFileChangeEvent) {
 					LOGGER.info(
 							"[SCT Config]  received polaris config change event and will refresh spring context."
-									+ "namespace = {}, group = {}, fileName = {}",
+									+ " namespace = {}, group = {}, fileName = {}",
 							polarisPropertySource.getNamespace(), polarisPropertySource.getGroup(),
 							polarisPropertySource.getFileName());
 
