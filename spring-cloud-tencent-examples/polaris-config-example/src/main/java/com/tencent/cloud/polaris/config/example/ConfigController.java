8eec5cc58c15aafdc426d09db2b66e444fd1d10b
lepdou <ledouzhang@tencent.com>
2022-09-09 16:00:06 +0800

    optimize config demo (#580)

--- spring-cloud-tencent-examples/polaris-config-example/src/main/java/com/tencent/cloud/polaris/config/example/ConfigController.java
+++ spring-cloud-tencent-examples/polaris-config-example/src/main/java/com/tencent/cloud/polaris/config/example/ConfigController.java
@@ -18,7 +18,6 @@ package com.tencent.cloud.polaris.config.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,7 +28,6 @@ import org.springframework.web.bind.annotation.RestController;
  * @author lepdou 2022-03-10
  */
 @RestController
-@RefreshScope
 public class ConfigController {
 
 	@Value("${timeout:1000}")
