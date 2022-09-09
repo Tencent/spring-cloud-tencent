ddb6e1c5e69181ad6bb2b83324ba196b51f4f04a
lepdou <lepdou@gmail.com>
2022-09-09 15:32:57 +0800

    remove useless import

--- spring-cloud-tencent-examples/polaris-config-example/src/main/java/com/tencent/cloud/polaris/config/example/ConfigController.java
+++ spring-cloud-tencent-examples/polaris-config-example/src/main/java/com/tencent/cloud/polaris/config/example/ConfigController.java
@@ -18,7 +18,6 @@ package com.tencent.cloud.polaris.config.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
