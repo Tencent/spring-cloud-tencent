1341be3c3190c4b69bd574487eeaaf790f29b187
pandaapo <1052156701@qq.com>
2022-09-28 06:26:20 +0800

    fix checkstyle error.

--- spring-cloud-starter-tencent-metadata-transfer/src/main/java/com/tencent/cloud/metadata/core/EncodeTransferMedataScgFilter.java
+++ spring-cloud-starter-tencent-metadata-transfer/src/main/java/com/tencent/cloud/metadata/core/EncodeTransferMedataScgFilter.java
@@ -25,7 +25,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 
 import com.tencent.cloud.common.constant.MetadataConstant;
