7a72fd63a6eb100f541036a340617d6dc049fc8f
Haotian Zhang <928016560@qq.com>
2022-10-26 14:42:29 +0800

    fix:fix transfer http headers not working bug. (#663)

--- spring-cloud-starter-tencent-metadata-transfer/src/main/java/com/tencent/cloud/metadata/core/EncodeTransferMedataRestTemplateInterceptor.java
+++ spring-cloud-starter-tencent-metadata-transfer/src/main/java/com/tencent/cloud/metadata/core/EncodeTransferMedataRestTemplateInterceptor.java
@@ -60,7 +60,7 @@ public class EncodeTransferMedataRestTemplateInterceptor implements ClientHttpRe
 		MetadataContext metadataContext = MetadataContextHolder.get();
 		Map<String, String> customMetadata = metadataContext.getCustomMetadata();
 		Map<String, String> disposableMetadata = metadataContext.getDisposableMetadata();
-		Map<String, String> transHeaders = metadataContext.getTransHeaders();
+		Map<String, String> transHeaders = metadataContext.getTransHeadersKV();
 
 		// build custom disposable metadata request header
 		this.buildMetadataHeader(httpRequest, disposableMetadata, CUSTOM_DISPOSABLE_METADATA);
