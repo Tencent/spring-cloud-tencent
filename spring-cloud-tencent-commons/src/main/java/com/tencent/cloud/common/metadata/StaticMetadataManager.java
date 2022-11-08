ca0846dc378c2fd73277fedcc6beead982079442
wulingxiao <1251605638@qqcom>
2022-11-08 21:54:21 +0800

    Test:support environment variable metadata test.

--- spring-cloud-tencent-commons/src/main/java/com/tencent/cloud/common/metadata/StaticMetadataManager.java
+++ spring-cloud-tencent-commons/src/main/java/com/tencent/cloud/common/metadata/StaticMetadataManager.java
@@ -13,7 +13,6 @@
  * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
  * CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
- *
  */
 
 package com.tencent.cloud.common.metadata;
@@ -109,8 +108,9 @@ public class StaticMetadataManager {
 			String value = entry.getValue();
 			if (StringUtils.isNotBlank(key)
 					&& (key.startsWith(ENV_METADATA_PREFIX) || key.equals(ENV_TRAFFIC_CONTENT_RAW_TRANSHEADERS))
-					&& !key.equals(ENV_METADATA_CONTENT_TRANSITIVE)) {
-				String sourceKey;
+					&& !key.equals(ENV_METADATA_CONTENT_TRANSITIVE)
+					&& !key.equals(ENV_METADATA_CONTENT_DISPOSABLE)) {
+				String sourceKey = "";
 				if (key.equals(ENV_TRAFFIC_CONTENT_RAW_TRANSHEADERS)) {
 					sourceKey = key;
 					envNotReportMetadata.put(sourceKey, value);
