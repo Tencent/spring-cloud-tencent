354da746db6a48028a0b4b813cd0d6e0cee5a498
Haotian Zhang <928016560@qq.com>
2023-01-31 11:36:53 +0800

    fix:nacos & polaris discovery (#820)
    Co-authored-by: liaochuntao <liaochuntao@live.com>

--- spring-cloud-tencent-commons/src/main/java/com/tencent/cloud/common/metadata/MetadataContext.java
+++ spring-cloud-tencent-commons/src/main/java/com/tencent/cloud/common/metadata/MetadataContext.java
@@ -24,6 +24,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import com.tencent.cloud.common.util.ApplicationContextAwareUtils;
+import com.tencent.cloud.common.util.DiscoveryUtil;
 import com.tencent.cloud.common.util.JacksonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -79,31 +80,36 @@ public class MetadataContext {
 	public static String LOCAL_SERVICE;
 
 	static {
-		String namespace = ApplicationContextAwareUtils.getProperties("spring.cloud.polaris.namespace");
-		if (StringUtils.isEmpty(namespace)) {
+		String namespace = ApplicationContextAwareUtils
+				.getProperties("spring.cloud.polaris.namespace");
+		if (!StringUtils.hasText(namespace)) {
 			namespace = ApplicationContextAwareUtils
 					.getProperties("spring.cloud.polaris.discovery.namespace", "default");
 		}
-		if (StringUtils.isEmpty(namespace)) {
+
+		if (!StringUtils.hasText(namespace)) {
 			LOG.error("namespace should not be blank. please configure spring.cloud.polaris.namespace or "
 					+ "spring.cloud.polaris.discovery.namespace");
 			throw new RuntimeException("namespace should not be blank. please configure spring.cloud.polaris.namespace or "
 					+ "spring.cloud.polaris.discovery.namespace");
 		}
+
 		LOCAL_NAMESPACE = namespace;
 
-		String serviceName = ApplicationContextAwareUtils.getProperties("spring.cloud.polaris.service");
-		if (StringUtils.isEmpty(serviceName)) {
+		String serviceName = ApplicationContextAwareUtils
+				.getProperties("spring.cloud.polaris.service");
+		if (!StringUtils.hasText(serviceName)) {
 			serviceName = ApplicationContextAwareUtils.getProperties(
 					"spring.cloud.polaris.discovery.service", ApplicationContextAwareUtils
 							.getProperties("spring.application.name", null));
 		}
-		if (StringUtils.isEmpty(serviceName)) {
+		if (!StringUtils.hasText(serviceName)) {
 			LOG.error("service name should not be blank. please configure spring.cloud.polaris.service or "
 					+ "spring.cloud.polaris.discovery.service or spring.application.name");
 			throw new RuntimeException("service name should not be blank. please configure spring.cloud.polaris.service or "
 					+ "spring.cloud.polaris.discovery.service or spring.application.name");
 		}
+		serviceName = DiscoveryUtil.rewriteServiceId(serviceName);
 		LOCAL_SERVICE = serviceName;
 	}
 
@@ -112,7 +118,6 @@ public class MetadataContext {
 	public MetadataContext() {
 		this.fragmentContexts = new ConcurrentHashMap<>();
 	}
-
 
 	public Map<String, String> getDisposableMetadata() {
 		return this.getFragmentContext(MetadataContext.FRAGMENT_DISPOSABLE);
