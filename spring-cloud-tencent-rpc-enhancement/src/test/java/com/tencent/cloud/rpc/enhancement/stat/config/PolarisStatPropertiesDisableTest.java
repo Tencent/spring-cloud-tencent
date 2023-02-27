e7f0148e5f20f391c880129e5a294987397d3c67
Haotian Zhang <928016560@qq.com>
2023-02-27 14:47:54 +0800

    feat:enable stat reporting as default.

---
+++ spring-cloud-tencent-rpc-enhancement/src/test/java/com/tencent/cloud/rpc/enhancement/stat/config/PolarisStatPropertiesDisableTest.java
@@ -0,0 +1,59 @@
+/*
+ * Tencent is pleased to support the open source community by making Spring Cloud Tencent available.
+ *
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.tencent.cloud.rpc.enhancement.stat.config;
+
+import com.tencent.polaris.client.api.SDKContext;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Test for {@link PolarisStatProperties}.
+ *
+ * @author Haotian Zhang
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = PolarisStatPropertiesDisableTest.TestApplication.class)
+@ActiveProfiles("disable")
+public class PolarisStatPropertiesDisableTest {
+
+	@Autowired
+	private PolarisStatProperties polarisStatProperties;
+
+	@Autowired
+	private SDKContext sdkContext;
+
+	@Test
+	public void testDefaultInitialization() {
+		assertThat(polarisStatProperties).isNotNull();
+		assertThat(polarisStatProperties.isEnabled()).isFalse();
+		assertThat(polarisStatProperties.getHost()).isBlank();
+	}
+
+	@SpringBootApplication
+	protected static class TestApplication {
+
+	}
+}
