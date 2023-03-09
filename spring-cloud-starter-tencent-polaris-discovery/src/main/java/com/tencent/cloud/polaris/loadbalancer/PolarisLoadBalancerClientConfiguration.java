1e7e63a875778ec10ac3d4c31db760053291870f
Haotian Zhang <928016560@qq.com>
2023-03-09 16:47:35 +0800

    fix:fix instance circuit breaker not working bug. (#893)

--- spring-cloud-starter-tencent-polaris-discovery/src/main/java/com/tencent/cloud/polaris/loadbalancer/PolarisLoadBalancerClientConfiguration.java
+++ spring-cloud-starter-tencent-polaris-discovery/src/main/java/com/tencent/cloud/polaris/loadbalancer/PolarisLoadBalancerClientConfiguration.java
@@ -20,15 +20,22 @@ package com.tencent.cloud.polaris.loadbalancer;
 import com.tencent.cloud.polaris.context.ConditionalOnPolarisEnabled;
 import com.tencent.polaris.router.api.core.RouterAPI;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.client.ConditionalOnBlockingDiscoveryEnabled;
 import org.springframework.cloud.client.ConditionalOnDiscoveryEnabled;
+import org.springframework.cloud.client.ConditionalOnReactiveDiscoveryEnabled;
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
 import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
 import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.core.env.Environment;
 
 /**
@@ -41,6 +48,11 @@ import org.springframework.core.env.Environment;
 @ConditionalOnDiscoveryEnabled
 public class PolarisLoadBalancerClientConfiguration {
 
+	/**
+	 * Order of reactive discovery service instance supplier.
+	 */
+	private static final int REACTIVE_SERVICE_INSTANCE_SUPPLIER_ORDER = 193827465;
+
 	private final static String STRATEGY_WEIGHT = "polarisWeighted";
 
 	@Bean
@@ -51,5 +63,36 @@ public class PolarisLoadBalancerClientConfiguration {
 		String name = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
 		return new PolarisLoadBalancer(name,
 				loadBalancerClientFactory.getLazyProvider(name, ServiceInstanceListSupplier.class), routerAPI);
+	}
+
+	@Configuration(proxyBeanMethods = false)
+	@ConditionalOnReactiveDiscoveryEnabled
+	@Order(REACTIVE_SERVICE_INSTANCE_SUPPLIER_ORDER)
+	protected static class PolarisReactiveSupportConfiguration {
+
+		@Bean
+		@ConditionalOnMissingBean
+		@ConditionalOnBean(ReactiveDiscoveryClient.class)
+		public ServiceInstanceListSupplier polarisRouterDiscoveryClientServiceInstanceListSupplier(
+				ConfigurableApplicationContext context) {
+			return new PolarisServiceInstanceListSupplier(
+					ServiceInstanceListSupplier.builder().withDiscoveryClient().build(context));
+		}
+
+	}
+
+	@Configuration(proxyBeanMethods = false)
+	@ConditionalOnBlockingDiscoveryEnabled
+	@Order(REACTIVE_SERVICE_INSTANCE_SUPPLIER_ORDER + 1)
+	protected static class PolarisBlockingSupportConfiguration {
+
+		@Bean
+		@ConditionalOnMissingBean
+		@ConditionalOnBean(DiscoveryClient.class)
+		public ServiceInstanceListSupplier polarisRouterDiscoveryClientServiceInstanceListSupplier(
+				ConfigurableApplicationContext context) {
+			return new PolarisServiceInstanceListSupplier(
+					ServiceInstanceListSupplier.builder().withBlockingDiscoveryClient().build(context));
+		}
 	}
 }
