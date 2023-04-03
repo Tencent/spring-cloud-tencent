fa108551da0eb65b9f72ee37320519deda8cd34b
atomzhong <atomzhong@tencent.com>
2023-04-03 15:16:13 +0800

    optimize code style.

--- spring-cloud-tencent-examples/polaris-discovery-example/discovery-callee-service/src/main/java/com/tencent/cloud/polaris/discovery/service/callee/DiscoveryCalleeController.java
+++ spring-cloud-tencent-examples/polaris-discovery-example/discovery-callee-service/src/main/java/com/tencent/cloud/polaris/discovery/service/callee/DiscoveryCalleeController.java
@@ -19,6 +19,7 @@ package com.tencent.cloud.polaris.discovery.service.callee;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
