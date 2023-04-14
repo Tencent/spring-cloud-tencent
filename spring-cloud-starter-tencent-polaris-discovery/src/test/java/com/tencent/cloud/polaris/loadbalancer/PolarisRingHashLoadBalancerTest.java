002a2888d96c14b2e8b4caf89bd7ee9bc3327f4a
seanyu <seansyyu@tencent.com>
2023-04-14 11:26:48 +0800

    fix ut

--- spring-cloud-starter-tencent-polaris-discovery/src/test/java/com/tencent/cloud/polaris/loadbalancer/PolarisRingHashLoadBalancerTest.java
+++ spring-cloud-starter-tencent-polaris-discovery/src/test/java/com/tencent/cloud/polaris/loadbalancer/PolarisRingHashLoadBalancerTest.java
@@ -21,6 +21,8 @@ package com.tencent.cloud.polaris.loadbalancer;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.tencent.cloud.common.metadata.StaticMetadataManager;
+import com.tencent.cloud.common.metadata.config.MetadataLocalProperties;
 import com.tencent.cloud.common.pojo.PolarisServiceInstance;
 import com.tencent.cloud.common.util.ApplicationContextAwareUtils;
 import com.tencent.polaris.api.pojo.Instance;
@@ -43,11 +45,14 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.Request;
 import org.springframework.cloud.client.loadbalancer.Response;
 import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.context.ApplicationContext;
 
 import static com.tencent.cloud.common.metadata.MetadataContext.LOCAL_NAMESPACE;
 import static com.tencent.cloud.common.metadata.MetadataContext.LOCAL_SERVICE;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -71,7 +76,13 @@ public class PolarisRingHashLoadBalancerTest {
 		mockedApplicationContextAwareUtils = Mockito.mockStatic(ApplicationContextAwareUtils.class);
 		mockedApplicationContextAwareUtils.when(() -> ApplicationContextAwareUtils.getProperties(anyString()))
 				.thenReturn("unit-test");
-
+		ApplicationContext applicationContext = mock(ApplicationContext.class);
+		MetadataLocalProperties metadataLocalProperties = mock(MetadataLocalProperties.class);
+		StaticMetadataManager staticMetadataManager = mock(StaticMetadataManager.class);
+		doReturn(metadataLocalProperties).when(applicationContext).getBean(MetadataLocalProperties.class);
+		doReturn(staticMetadataManager).when(applicationContext).getBean(StaticMetadataManager.class);
+		mockedApplicationContextAwareUtils.when(ApplicationContextAwareUtils::getApplicationContext)
+				.thenReturn(applicationContext);
 		testInstance = Instance.createDefaultInstance("instance-id", LOCAL_NAMESPACE,
 				LOCAL_SERVICE, "host", 8090);
 	}
