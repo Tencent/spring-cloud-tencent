66a4f7ef67ad19398987194c28be1fb2819f248a
Haotian Zhang <928016560@qq.com>
2023-08-02 19:35:38 +0800

    refactor:optimize discovery properties initialization.

--- spring-cloud-starter-tencent-polaris-discovery/src/test/java/com/tencent/cloud/polaris/DiscoveryPropertiesAutoConfigurationTest.java
+++ spring-cloud-starter-tencent-polaris-discovery/src/test/java/com/tencent/cloud/polaris/DiscoveryPropertiesAutoConfigurationTest.java
@@ -24,8 +24,6 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -49,22 +47,5 @@ public class DiscoveryPropertiesAutoConfigurationTest {
 			assertThat(context).hasSingleBean(PolarisDiscoveryHandler.class);
 			assertThat(context).hasSingleBean(DiscoveryConfigModifier.class);
 		});
-	}
-
-	@Configuration
-	static class TestConfiguration {
-		@Bean
-		public PolarisDiscoveryProperties polarisDiscoveryProperties() {
-			PolarisDiscoveryProperties polarisDiscoveryProperties = new PolarisDiscoveryProperties();
-			polarisDiscoveryProperties.setEnabled(false);
-			return polarisDiscoveryProperties;
-		}
-
-		@Bean
-		public ConsulContextProperties consulContextProperties() {
-			ConsulContextProperties consulContextProperties = new ConsulContextProperties();
-			consulContextProperties.setEnabled(true);
-			return consulContextProperties;
-		}
 	}
 }
