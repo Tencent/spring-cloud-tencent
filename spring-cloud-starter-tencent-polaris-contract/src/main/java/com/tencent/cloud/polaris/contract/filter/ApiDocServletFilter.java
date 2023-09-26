672bbf772731bed13d8f09722722180f241d2808
Haotian Zhang <928016560@qq.com>
2023-09-26 13:12:24 +0800

    feat:add swagger exposure filters.

--- spring-cloud-starter-tencent-polaris-contract/src/main/java/com/tencent/cloud/polaris/contract/filter/ApiDocServletFilter.java
+++ spring-cloud-starter-tencent-polaris-contract/src/main/java/com/tencent/cloud/polaris/contract/filter/ApiDocServletFilter.java
@@ -22,7 +22,6 @@ import java.io.IOException;
 import com.tencent.cloud.polaris.contract.config.PolarisContractProperties;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -30,18 +29,17 @@ import org.springframework.lang.NonNull;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import static com.tencent.cloud.polaris.contract.filter.FilterConstant.SWAGGER_RESOURCE_PREFIX;
+import static com.tencent.cloud.polaris.contract.filter.FilterConstant.SWAGGER_UI_URL;
 import static com.tencent.cloud.polaris.contract.filter.FilterConstant.SWAGGER_V2_API_DOC_URL;
 import static com.tencent.cloud.polaris.contract.filter.FilterConstant.SWAGGER_V3_API_DOC_URL;
 import static com.tencent.cloud.polaris.contract.filter.FilterConstant.SWAGGER_WEBJARS_V2_PREFIX;
 import static com.tencent.cloud.polaris.contract.filter.FilterConstant.SWAGGER_WEBJARS_V3_PREFIX;
-import static org.springdoc.core.utils.Constants.SWAGGER_UI_URL;
 
 /**
  * Filter to disable api doc controller.
  *
  * @author Haotian Zhang
  */
-@WebFilter
 public class ApiDocServletFilter extends OncePerRequestFilter {
 
 	private final PolarisContractProperties polarisContractProperties;
