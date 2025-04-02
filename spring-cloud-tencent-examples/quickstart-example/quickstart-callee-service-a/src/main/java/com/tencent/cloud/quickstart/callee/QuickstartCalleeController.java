9d0ec2a9a14fe565c93259732da7ed785bb21d01
Haotian Zhang <skyebefreeman@qq.com>
2025-04-02 11:54:49 +0800

    docs:update JDK version configuration in GitHub Actions. (#1528)

--- spring-cloud-tencent-examples/quickstart-example/quickstart-callee-service-a/src/main/java/com/tencent/cloud/quickstart/callee/QuickstartCalleeController.java
+++ spring-cloud-tencent-examples/quickstart-example/quickstart-callee-service-a/src/main/java/com/tencent/cloud/quickstart/callee/QuickstartCalleeController.java
@@ -28,6 +28,7 @@ import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -42,6 +43,7 @@ import static com.tencent.cloud.common.constant.ContextConstant.UTF_8;
  *
  * @author Haotian Zhang
  */
+@RefreshScope
 @RestController
 @RequestMapping("/quickstart/callee")
 public class QuickstartCalleeController {
