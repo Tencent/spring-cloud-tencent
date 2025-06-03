38b57b1b5dfdaf5565317cb7f1789f9ad604ac57
Haotian Zhang <928016560@qq.com>
2025-06-03 15:37:24 +0800

    feat:support ipv6.

--- spring-cloud-starter-tencent-polaris-discovery/src/test/java/com/tencent/cloud/polaris/PolarisDiscoveryPropertiesTest.java
+++ spring-cloud-starter-tencent-polaris-discovery/src/test/java/com/tencent/cloud/polaris/PolarisDiscoveryPropertiesTest.java
@@ -102,6 +102,7 @@ public class PolarisDiscoveryPropertiesTest {
 						+ ", healthCheckUrl='/health'"
 						+ ", serviceListRefreshInterval=1000"
 						+ ", zeroProtectionEnabled=false"
-						+ ", zeroProtectionNeedTestConnectivity=false}");
+						+ ", zeroProtectionNeedTestConnectivity=false"
+						+ ", preferIpv6=false}");
 	}
 }
