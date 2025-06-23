8d1a083cb7293807ccab6d3483968fc4360d5a05
Fishtail <49390359+fuyuwei01@users.noreply.github.com>
2025-06-23 13:10:32 +0800

    feat:support polaris server nearby backup LB. (#1628)
    Co-authored-by: Haotian Zhang <skyebefreeman@qq.com>

--- spring-cloud-tencent-polaris-context/src/main/java/com/tencent/cloud/polaris/context/ModifyAddress.java
+++ spring-cloud-tencent-polaris-context/src/main/java/com/tencent/cloud/polaris/context/ModifyAddress.java
@@ -26,6 +26,7 @@ import com.tencent.cloud.polaris.context.config.PolarisContextProperties;
 import com.tencent.polaris.api.utils.CollectionUtils;
 import com.tencent.polaris.api.utils.StringUtils;
 import com.tencent.polaris.factory.config.ConfigurationImpl;
+import com.tencent.polaris.factory.config.global.ServerConnectorConfigImpl;
 
 /**
  * Modify polaris server address.
@@ -48,7 +49,10 @@ public class ModifyAddress implements PolarisConfigModifier {
 
 		List<String> addresses = AddressUtils.parseAddressList(properties.getAddress());
 
-		configuration.getGlobal().getServerConnector().setAddresses(addresses);
+		ServerConnectorConfigImpl serverConnectorConfig = configuration.getGlobal().getServerConnector();
+		serverConnectorConfig.setAddresses(addresses);
+		serverConnectorConfig.setLbPolicy(properties.getAddressLbPolicy());
+		serverConnectorConfig.setServerSwitchInterval(properties.getServerSwitchInterval());
 		if (CollectionUtils.isEmpty(configuration.getGlobal().getServerConnectors())) {
 			configuration.getGlobal().setServerConnectors(new ArrayList<>());
 		}
