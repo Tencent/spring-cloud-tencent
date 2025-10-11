36c290bf492625d71f4a3a771005c6f6e43e3596
fishtailfu <fishtailfu@tencent.com>
2025-10-11 16:12:01 +0800

    fix: fix quickstart

--- spring-cloud-tencent-examples/quickstart-example/quickstart-callee-service-b/src/main/java/com/tencent/cloud/quickstart/callee/CustomMetadata.java
+++ spring-cloud-tencent-examples/quickstart-example/quickstart-callee-service-b/src/main/java/com/tencent/cloud/quickstart/callee/CustomMetadata.java
@@ -36,7 +36,6 @@ public class CustomMetadata implements InstanceMetadataProvider {
 	public Map<String, String> getMetadata() {
 		Map<String, String> metadata = new HashMap<>();
 		metadata.put("k1", "v2");
-		metadata.put("lane", "lane2");
 		return metadata;
 	}
 
