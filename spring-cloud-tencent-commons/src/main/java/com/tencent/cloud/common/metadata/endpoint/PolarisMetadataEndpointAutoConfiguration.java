b80a5f266a73d14ae9453d16ccf1f22abb58a6b3
KIRA_Z <1176382673@qq.com>
2025-11-18 21:53:03 +0800

    fix:Remove invalid imports(#1749)

--- spring-cloud-tencent-commons/src/main/java/com/tencent/cloud/common/metadata/endpoint/PolarisMetadataEndpointAutoConfiguration.java
+++ spring-cloud-tencent-commons/src/main/java/com/tencent/cloud/common/metadata/endpoint/PolarisMetadataEndpointAutoConfiguration.java
@@ -20,7 +20,6 @@ package com.tencent.cloud.common.metadata.endpoint;
 import com.tencent.cloud.common.metadata.StaticMetadataManager;
 
 import org.springframework.boot.actuate.autoconfigure.endpoint.condition.ConditionalOnAvailableEndpoint;
-import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
