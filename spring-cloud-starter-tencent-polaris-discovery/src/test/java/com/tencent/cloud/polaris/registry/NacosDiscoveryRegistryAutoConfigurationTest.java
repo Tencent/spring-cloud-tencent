b8229763869404d0cccd771a6ec11a303cac8dd0
fishtailfu <fishtailfu@tencent.com>
2026-01-12 17:24:57 +0800

    fix: fix NacosDiscoveryRegistryAutoConfiguration and PolarisAuthAutoConfiguration error initiation when polaris disabled.

--- spring-cloud-starter-tencent-polaris-discovery/src/test/java/com/tencent/cloud/polaris/registry/NacosDiscoveryRegistryAutoConfigurationTest.java
+++ spring-cloud-starter-tencent-polaris-discovery/src/test/java/com/tencent/cloud/polaris/registry/NacosDiscoveryRegistryAutoConfigurationTest.java
@@ -26,8 +26,6 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
